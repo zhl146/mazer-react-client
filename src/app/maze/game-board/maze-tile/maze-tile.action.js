2cc142952fb35498be70015d150a6a52dfae783e
zhen lu <zhen_lu@comcast.com>
2018-07-01 16:00:41 -0400

    fix eslint

--- src/app/maze/game-board/maze-tile/maze-tile.action.js
+++ src/app/maze/game-board/maze-tile/maze-tile.action.js
@@ -1,5 +1,5 @@
-import { createUpdateAction } from '../../../../utils/action-creator'
-import { UPDATE_MAZE } from '../../../../store/action-constants'
+import { createUpdateAction } from 'utils/action-creator'
+import { UPDATE_MAZE } from 'store/action-constants'
 
 export const doAction = tile => createUpdateAction(UPDATE_MAZE, tile)
 
