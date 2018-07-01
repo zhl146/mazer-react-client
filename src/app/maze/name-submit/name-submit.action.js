423990cf59c5962af04d23626e5778029a827000
zhen lu <zhen_lu@comcast.com>
2018-07-01 19:05:42 -0400

    remove console

--- src/app/maze/name-submit/name-submit.action.js
+++ src/app/maze/name-submit/name-submit.action.js
@@ -3,7 +3,6 @@ import axios from 'axios'
 import { solutionUrl } from '../../../server/url-generator'
 
 export const submitScore = (name, history) => async (dispatch, getState) => {
-  console.log(getState())
   let { mazeState, auth } = getState()
 
   let payload = {
@@ -14,7 +13,6 @@ export const submitScore = (name, history) => async (dispatch, getState) => {
   }
   try {
     let response = await axios.post(solutionUrl, payload)
-    console.log(response)
     history.push('/leaderboard?seed=' + mazeState.maze.seed)
   } catch (e) {
     console.log(e)
