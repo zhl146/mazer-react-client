f2ed4506922cfe5e47b08e455d8e9d9b5ea9c0d7
chaosed0 <chaosed0@gmail.com>
2018-07-02 10:01:01 -0700

    Add progress indicator component

--- src/utils/color.js
+++ src/utils/color.js
@@ -3,9 +3,9 @@ export const Color = (r, g, b, a) => ({ r, g, b, a: a || 255 })
 export const ColorToString = ({r,g,b,a}) => `rgba(${r}, ${b}, ${g}, ${a})`
 
 export const InterpolateColor = (color1, color2, time) => {
-  const r = color1.r * (1 - time) + color2.r * time
-  const g = color1.g * (1 - time) + color2.b * time
-  const b = color1.g * (1 - time) + color2.b * time
-  const a = color1.a * (1 - time) + color2.a * time
+  const r = Interpolate(color1.r, color2.r, time)
+  const g = Interpolate(color1.g, color2.g, time)
+  const b = Interpolate(color1.b, color2.b, time)
+  const a = Interpolate(color1.a, color2.a, time)
   return Color(r,g,b,a);
 }
